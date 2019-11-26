b0b657c0f327678e8940b775fe24912a0040cd36
惠维乐 <weiller@126.com>
2019-11-26 17:25:42 +0800

    mq案例代码

---
+++ mp-producer/src/main/java/com/weiller/mp/producer/MpProducerApplication.java
@@ -0,0 +1,13 @@
+package com.weiller.mp.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpProducerApplication.class, args);
+	}
+
+}
---
+++ mp-producer/src/test/java/com/weiller/mp/producer/MpProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weiller.mp.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
